805aecb8f4dd16c510cbb4b8442ef548cb5a3a40
heecircle <yhw991228@gmail.com>
2025-04-01 14:39:56 +0900

    🎉 [feat] Initial Commit

---
+++ src/main/java/com/heewon/ical/IcalApplication.java
@@ -0,0 +1,13 @@
+package com.heewon.ical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heewon/ical/IcalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heewon.ical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
